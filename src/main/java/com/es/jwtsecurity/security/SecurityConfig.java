cbc7e3cec65322dd357597d40125e78a730d1591
lkru <lkru144@g.educaand.es>
2024-11-27 21:27:50 +0100

    juan

---
+++ src/main/java/com/es/jwtsecurity/security/SecurityConfig.java
@@ -0,0 +1,72 @@
+package com.es.jwtsecurity.security;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private RsaKeyProperties rsaKeyProperties;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http
+    ) throws Exception {
+
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/usuarios/login", "/usuarios/register").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults())
+                .build();
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(rsaKeyProperties.publicKey()).build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(rsaKeyProperties.publicKey()).privateKey(rsaKeyProperties.privateKey()).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
+}
--- src/main/java/com/es/jwtsecurity/service/CustomUserDetailsService.java
+++ src/main/java/com/es/jwtsecurity/service/CustomUserDetailsService.java
@@ -1,7 +1,71 @@
 package com.es.jwtsecurity.service;
 
+import com.es.jwtsecurity.dto.UsuarioRegisterDTO;
+import com.es.jwtsecurity.model.Usuario;
+import com.es.jwtsecurity.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+// es el user service
+
 @Service
-public class CustomUserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        // BUSCO EL USUARIO POR SU NOMBRE EN LA BDD
+        Usuario usuario = usuarioRepository
+                .findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario No encontrado"));
+
+        /* RETORNAMOS UN USERDETAILS
+        El método loadUserByUsername nos fuerza a devolver un objeto de tipo UserDetails.
+        Tenemos que convertir nuestro objeto de tipo Usuario a un objeto de tipo UserDetails
+        ¡No os preocupéis, esto es siempre igual!
+         */
+        UserDetails userDetails = User // User pertenece a SpringSecurity
+                .builder()
+                .username(usuario.getUsername())
+                .password(usuario.getPassword())
+                .roles(usuario.getRoles().split(","))
+                .build();
+
+        return userDetails;
+    }
+
+    public UsuarioRegisterDTO registerUser(UsuarioRegisterDTO usuarioRegisterDTO) {
+        // Comprobamos que el usuario no existe en la base de datos
+        if (usuarioRepository.findByUsername(usuarioRegisterDTO.getUsername()).isPresent()) {
+            throw new IllegalArgumentException("El nombre de usuario ya existe");
+        }
+
+        // Creamos la instancia de
+        Usuario newUsuario = new Usuario();
+
+        /*
+         La password del newUsuario debe estar hasheada, así que usamos el passwordEncoder que tenemos definido.
+         ¿De dónde viene ese passwordEncoder?
+         El objeto passwordEncoder está definido al principio de esta clase.
+         */
+        newUsuario.setPassword(passwordEncoder.encode(usuarioRegisterDTO.getPassword())); // Hashear la contraseña
+        newUsuario.setUsername(usuarioRegisterDTO.getUsername());
+        newUsuario.setRoles(usuarioRegisterDTO.getRoles());
+
+        // Guardamos el newUsuario en la base de datos... igual que siempre
+        usuarioRepository.save(newUsuario);
+
+        return usuarioRegisterDTO;
+    }
 }
